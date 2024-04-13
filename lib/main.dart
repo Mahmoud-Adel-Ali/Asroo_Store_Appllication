f41a67c2e66f42a2e81d0f6800cb0bf518931d7e
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-04-12 23:34:34 -0700

    firebase andriod setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+
 import 'package:asroo_store/asroo_store_app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Platform.isAndroid
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AsrooStoreApp());
 }
-
