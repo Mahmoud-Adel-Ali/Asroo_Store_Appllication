3d039b42aed9fdff169b666ef20f3f71da3c66a6
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-04-14 19:44:53 -0700

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
-
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/env_variable.dart';
 import 'package:asroo_store/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Platform.isAndroid
 
+  await EnvVariable.instance.init(envTypeEnum: EnvTypeEnum.dev);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const AsrooStoreApp());
+  // this step used to set device not rotated
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const AsrooStoreApp());
+  });
 }
