c0d67c2bba83a6efd3274f8f86de57149ad61aab
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-04-24 14:55:04 -0700

    setup freezed , bloc and shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/app/bloc_observer.dart';
 import 'package:asroo_store/core/app/env_variable.dart';
 import 'package:asroo_store/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Platform.isAndroid
 
   await EnvVariable.instance.init(envTypeEnum: EnvTypeEnum.dev);
-
+  Bloc.observer = AppBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
