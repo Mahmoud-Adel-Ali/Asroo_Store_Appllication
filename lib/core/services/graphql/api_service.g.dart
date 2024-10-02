184c82a4eda44c0e6717eda428375bdca7701309
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-01 17:02:52 -0700

    ..........................

--- lib/core/services/graphql/api_service.g.dart
+++ lib/core/services/graphql/api_service.g.dart
@@ -6,6 +6,7 @@ part of 'api_service.dart';
 // RetrofitGenerator
 // **************************************************************************
 
+// ignore: lines_longer_than_80_chars
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
 
 class _ApiService implements ApiService {
